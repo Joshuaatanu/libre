994386d14aebbe547cad4e7d10ddfebc2c4043ae
Joshua Atanu <atanu.joshua@gmail.com>
2023-07-25 20:51:48 +0100

    test

--- pages/api/unfavorite.ts
+++ pages/api/unfavorite.ts
@@ -1,58 +1,58 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import prismadb from '@/lib/prismadb';
+// import { NextApiRequest, NextApiResponse } from "next";
+// import prismadb from '@/lib/prismadb';
 
-import { getSession } from "next-auth/react";
-import { without } from "lodash";
+// import { getSession } from "next-auth/react";
+// import { without } from "lodash";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    if (req.method !== 'POST') { //get request
-      return res.status(405).end();
-    }
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   try {
+//     if (req.method !== 'POST') { //get request
+//       return res.status(405).end();
+//     }
 
-    const session = await getSession({ req });
+//     const session = await getSession({ req });
 
-    if (!session?.user?.email) {
-      throw new Error('Not signed in');
-    }
+//     if (!session?.user?.email) {
+//       throw new Error('Not signed in');
+//     }
 
-    const { bookId } = req.body;
+//     const { bookId } = req.body;
 
-    const existingMovie = await prismadb.book.findUnique({
-      where: {
-        id: bookId,
-      }
-    });
+//     const existingMovie = await prismadb.book.findUnique({
+//       where: {
+//         id: bookId,
+//       }
+//     });
 
-    if (!existingMovie) {
-      throw new Error('Invalid ID');
-    }
+//     if (!existingMovie) {
+//       throw new Error('Invalid ID');
+//     }
 
-    const user = await prismadb.user.findUnique({
-      where: {
-        email: session.user.email,
-      },
-    });
+//     const user = await prismadb.user.findUnique({
+//       where: {
+//         email: session.user.email,
+//       },
+//     });
 
-    if (!user) {
-      throw new Error('Invalid email');
-    }
+//     if (!user) {
+//       throw new Error('Invalid email');
+//     }
 
-    const updatedFavoriteIds = without(user.favoriteIds, bookId);
+//     const updatedFavoriteIds = without(user.favoriteIds, bookId);
 
-    const updatedUser = await prismadb.user.update({
-      where: {
-        email: session.user.email,
-      },
-      data: {
-        favoriteIds: updatedFavoriteIds,
-      }
-    });
+//     const updatedUser = await prismadb.user.update({
+//       where: {
+//         email: session.user.email,
+//       },
+//       data: {
+//         favoriteIds: updatedFavoriteIds,
+//       }
+//     });
 
-    return res.status(200).json(updatedUser);
-  } catch (error) {
-    console.log(error);
+//     return res.status(200).json(updatedUser);
+//   } catch (error) {
+//     console.log(error);
 
-    return res.status(500).end();
-  }
-}
+//     return res.status(500).end();
+//   }
+// }
